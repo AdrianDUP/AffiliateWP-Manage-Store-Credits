cf5aba8420543661e09144d8ad3f690782ed8650
Adrian Du Plessis <adrian@graphiteprogramming.com>
2015-10-20 14:22:58 +0200

    Added the initial plugin loading file.

---
+++ affiliatewp-manage-store-credits/affilaitewp-manage-store-credits.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: AffiliateWP Manage Store Credits
+ * Plugin URI: https://github.com/Jogger71/AffiliateWP-Manage-Store-Credits
+ * Author: Adrian Du Plessis
+ * Author URI: https://github.com/Jogger71/
+ *
+ * Description: A lightweight plugin that allows you to manage the amounts of store credits of your affiliates. It also incorporates the AffiliateWP Store Credits Display Plugin
+ *
+ * Version: 0.0.0
+ */
